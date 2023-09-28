d0d84b331d2254ae268775cc511bc7faf628592c
Devin Spaulding <109320147+spaulding42@users.noreply.github.com>
2023-09-28 00:16:05 -0400

    css

--- index.js
+++ index.js
@@ -3,7 +3,36 @@ window.addEventListener("scroll", function(){
     nav.classList.toggle("nav-fill", window.scrollY > 0);
 })
 
-window.addEventListener("mousedown", function(e){
-    console.log(this);
-    var clicked = document.querySelector(this.src);
-})
+function shrink(e){
+    var enlargedPic = document.getElementById("enlarged-pic")
+    var bg = document.getElementById("black-overlay")
+    bg.hidden = true
+    enlargedPic.src = ""
+    enlargedPic.hidden = true
+}
+function enlarge(e)
+{
+    console.log(e.src)
+    var enlargedPic = document.getElementById("enlarged-pic")
+    var bg = document.getElementById("black-overlay")
+    bg.hidden = false
+    enlargedPic.src = e.src
+    enlargedPic.hidden = false
+
+    // make the alt text show up on enlarged image as a description
+    var picText = document.getElementById("enlarged-alt")
+    picText.innerHTML = e.alt
+}
+
+
+function selfieIn(e)
+{
+    console.log(e.src)
+    e.src = "Assets/Tahoe_Selfie.jpg";
+}
+
+function selfieOut(e)
+{
+    console.log("OUT")
+    e.src = "Assets/Tahoe_Selfie-BnW.jpg";
+}
